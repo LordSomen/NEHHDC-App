d98623707e34e2feeb9c35e8161260c09211aefd
LordSomen <soumyajit637@gmail.com>
2018-01-16 04:56:59 +0530

    completed the ui partially for the cart

---
+++ app/src/main/java/nehhdc/android/com/nehhdc_app/CartActivity.java
@@ -0,0 +1,13 @@
+package nehhdc.android.com.nehhdc_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
